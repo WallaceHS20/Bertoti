fea8a87e2fa346f9be45b32749210cef320654f1
Wallace Honorato da Silva <101594950+WallaceHS20@users.noreply.github.com>
2023-11-27 17:30:02 -0300

    Delete Padroes de Projetos/src/MVC directory

--- Padroes de Projetos/src/MVC/src/CircleStrategy.java
+++
@@ -1,9 +0,0 @@
-import java.awt.*;
-
-// Concrete Strategies para diferentes formas
-public class CircleStrategy implements DrawingStrategy {
-    @Override
-    public void draw(Graphics g) {
-        g.drawOval(10, 10, 100, 100); // Exemplo de parâmetros
-    }
-}
